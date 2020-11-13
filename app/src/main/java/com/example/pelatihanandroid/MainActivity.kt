a997ebebe5662c3bb2c0c8c9137c7743e56e88dd
Benaya Caesario Perdana <coolcool.rio75@gmail.com>
2020-11-13 15:52:12 +0700

    branch minggu 3

--- app/src/main/java/com/example/pelatihanandroid/MainActivity.kt
+++ app/src/main/java/com/example/pelatihanandroid/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.pelatihanandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,16 +13,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getStartedButton.setOnClickListener {
-            val email = emailAddressEditText.text.toString()
-            val bla: String
-            if(email.isEmpty()) {
-                Toast.makeText(this, "Please enter your email address", Toast.LENGTH_SHORT).show()
-            } else {
-                val intent = Intent(this, NextActivity::class.java)
-                intent.putExtra("SEND_EMAIL", email)
-                startActivity(intent)
-            }
+        facebookButton.setOnClickListener {
+            Toast.makeText(applicationContext, "Facebook", Toast.LENGTH_SHORT).show()
         }
+        appleButton.setOnClickListener {
+            Toast.makeText(applicationContext, "Apple", Toast.LENGTH_SHORT).show()
+
+        }
+
+        googleButton.setOnClickListener {
+            Toast.makeText(applicationContext, "Google", Toast.LENGTH_SHORT).show()
+        }
+
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, ShowDataActivity::class.java)
+            val data = getEditText()
+            intent.putExtra("EXTRA_DATA", data)
+            startActivity(intent)
+        }
+    }
+
+    fun getEditText(): String {
+        return emailAddressEditText.text.toString()
     }
 }
